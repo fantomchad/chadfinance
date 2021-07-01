4b8443a0562e158275d6e3bf9935c3880b1c5bb6
exitonoff <exitonoff@gmail.com>
2021-07-01 13:00:27 +0300

    Remove broken imports

--- src/pages/Farms.tsx
+++ src/pages/Farms.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useWallet } from 'use-wallet'
-import WalletBalance from '../components/WalletBalance'
 
 const Home: React.FC = () => {
   const wallet = useWallet()
