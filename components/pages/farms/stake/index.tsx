c1910f7b5155634b3dcee91ea00c4c9edc44d291
exitonoff <exitonoffdev@gmail.com>
2021-07-08 13:23:50 +0300

    Some refactoring

--- components/pages/farms/stake/index.tsx
+++ components/pages/farms/stake/index.tsx
@@ -10,11 +10,11 @@ import { ethers, providers } from "ethers"
 
 
 function numFormatter(num) {
-    if(num > 999 && num < 1000000){
-        return (num/1000).toFixed(1) + 'K'; // convert to K for number from > 1000 < 1 million 
-    }else if(num > 1000000){
-        return (num/1000000).toFixed(1) + 'M'; // convert to M for number from > 1 million 
-    }else if(num < 900){
+    if (num > 999 && num < 1000000) {
+        return (num / 1000).toFixed(1) + 'K'; // convert to K for number from > 1000 < 1 million 
+    } else if (num > 1000000) {
+        return (num / 1000000).toFixed(1) + 'M'; // convert to M for number from > 1 million 
+    } else if (num < 900) {
         return num; // if value < 1000, nothing to do
     }
 }
@@ -26,10 +26,6 @@ export function Stake({ farm }) {
     const [toggle, setToggle] = useState(false)
     const [current, setCurrent] = useState(farm)
     const [lpTokenName, setLpTokenName] = useState('')
-    let [chadEarned, setChadEarned] = useState('')
-
-
-
 
     const getLPInfo = async () => {
         if (active) {
@@ -41,20 +37,19 @@ export function Stake({ farm }) {
             const chadMasterABI = [{ "inputs": [{ "internalType": "contract ChadFinanceToken", "name": "_CHAD", "type": "address" }, { "internalType": "address", "name": "_devaddr", "type": "address" }, { "internalType": "address", "name": "_feeAddress", "type": "address" }, { "internalType": "uint256", "name": "_ChadPerBlock", "type": "uint256" }, { "internalType": "uint256", "name": "_startBlock", "type": "uint256" }], "stateMutability": "nonpayable", "type": "constructor" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "user", "type": "address" }, { "indexed": true, "internalType": "uint256", "name": "pid", "type": "uint256" }, { "indexed": false, "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "Deposit", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "user", "type": "address" }, { "indexed": true, "internalType": "uint256", "name": "pid", "type": "uint256" }, { "indexed": false, "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "EmergencyWithdraw", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "previousOwner", "type": "address" }, { "indexed": true, "internalType": "address", "name": "newOwner", "type": "address" }], "name": "OwnershipTransferred", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "user", "type": "address" }, { "indexed": true, "internalType": "uint256", "name": "pid", "type": "uint256" }, { "indexed": false, "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "Withdraw", "type": "event" }, { "inputs": [], "name": "BONUS_MULTIPLIER", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "CHAD", "outputs": [{ "internalType": "contract ChadFinanceToken", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "ChadPerBlock", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "_allocPoint", "type": "uint256" }, { "internalType": "contract IBEP20", "name": "_lpToken", "type": "address" }, { "internalType": "uint16", "name": "_depositFeeBP", "type": "uint16" }, { "internalType": "bool", "name": "_withUpdate", "type": "bool" }], "name": "add", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "_pid", "type": "uint256" }, { "internalType": "uint256", "name": "_amount", "type": "uint256" }], "name": "deposit", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "_devaddr", "type": "address" }], "name": "dev", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "devaddr", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "_pid", "type": "uint256" }], "name": "emergencyWithdraw", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "feeAddress", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "_from", "type": "uint256" }, { "internalType": "uint256", "name": "_to", "type": "uint256" }], "name": "getMultiplier", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "massUpdatePools", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "owner", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "_pid", "type": "uint256" }, { "internalType": "address", "name": "_user", "type": "address" }], "name": "pendingChad", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "name": "poolInfo", "outputs": [{ "internalType": "contract IBEP20", "name": "lpToken", "type": "address" }, { "internalType": "uint256", "name": "allocPoint", "type": "uint256" }, { "internalType": "uint256", "name": "lastRewardBlock", "type": "uint256" }, { "internalType": "uint256", "name": "accChadPerShare", "type": "uint256" }, { "internalType": "uint16", "name": "depositFeeBP", "type": "uint16" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "poolLength", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "renounceOwnership", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "_pid", "type": "uint256" }, { "internalType": "uint256", "name": "_allocPoint", "type": "uint256" }, { "internalType": "uint16", "name": "_depositFeeBP", "type": "uint16" }, { "internalType": "bool", "name": "_withUpdate", "type": "bool" }], "name": "set", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "_feeAddress", "type": "address" }], "name": "setFeeAddress", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "startBlock", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "totalAllocPoint", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "newOwner", "type": "address" }], "name": "transferOwnership", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "_ChadPerBlock", "type": "uint256" }], "name": "updateEmissionRate", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "_pid", "type": "uint256" }], "name": "updatePool", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }, { "internalType": "address", "name": "", "type": "address" }], "name": "userInfo", "outputs": [{ "internalType": "uint256", "name": "amount", "type": "uint256" }, { "internalType": "uint256", "name": "rewardDebt", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "_pid", "type": "uint256" }, { "internalType": "uint256", "name": "_amount", "type": "uint256" }], "name": "withdraw", "outputs": [], "stateMutability": "nonpayable", "type": "function" }]
             const chadMasterContract = new ethers.Contract(chadMaster, chadMasterABI, provider);
 
-            let chadEarned1 = await chadMasterContract.pendingChad(current.pid, account)
-            let formatBal = ethers.utils.formatUnits(chadEarned1, 18)
+            const chadEarned = await chadMasterContract.pendingChad(current.pid, account)
+            const formatBal = ethers.utils.formatUnits(chadEarned, 18)
             console.log(current.pid, ':', formatBal)
 
-            setChadEarned(formatBal)
-            var roundedBal = parseFloat(formatBal).toFixed(3)
+            const roundedEarnedBal = parseFloat(formatBal).toFixed(3)
 
-            current.earned = roundedBal
+            current.earned = roundedEarnedBal
 
             //Code to check the token allowance
             const lpAbi = [{ "inputs": [{ "internalType": "address", "name": "owner", "type": "address" }, { "internalType": "address", "name": "spender", "type": "address" }], "name": "allowance", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }]
             const lpContract = new ethers.Contract(current.lpTokenAddress, lpAbi, provider)
             const approved = await lpContract.allowance(account, chadMaster)
-            let formatapprove = ethers.utils.formatUnits(approved, 18)
+            const formatapprove = ethers.utils.formatUnits(approved, 18)
 
             //Enable Approve contract button, we do this so that when we switch wallets the approve button is reset.
             current.approved = false
@@ -65,29 +60,26 @@ export function Stake({ farm }) {
             }
             // console.log('approved:', current.pid, formatapprove)
 
-
             // LP Staked 
-            let lpStaked = await chadMasterContract.userInfo(current.pid, account).then(amount => {
-                let formattedLPStaked = ethers.utils.formatUnits(amount.amount, 18)
-                var roundedBal = parseFloat(formattedLPStaked).toFixed(2)
-                let abbreviated = numFormatter(roundedBal)
-                console.log('lpstaked ', current.pid, ':', abbreviated)
+            const lpStaked = await chadMasterContract.userInfo(current.pid, account)
+            
+            const formattedLPStaked = ethers.utils.formatUnits(lpStaked.amount, 18)
+            const parsedStakedBalance = parseFloat(formattedLPStaked)
+            const roundedStakedBalance = parsedStakedBalance.toFixed(2)
+            const abbreviated = numFormatter(roundedStakedBalance)
+            console.log('lpstaked ', current.pid, ':', abbreviated)
 
-                if ( abbreviated > 0 && abbreviated < 0.001){
-                    let smallValue = '< 0.001'
-                    current.staked = smallValue
-                }else{
-                    current.staked = abbreviated
-
-                }
-            })
-
+            if (parsedStakedBalance > 0 && parsedStakedBalance < 0.001) {
+                let smallValue = '< 0.001'
+                current.staked = smallValue
+            } else {
+                current.staked = abbreviated
+            }
+            
             //Update the farms data
             setCurrent(Object.assign({}, current))
         }
     }
-
-
 
     useEffect(() => {
         getLPInfo()
@@ -139,8 +131,6 @@ export function Stake({ farm }) {
         }
         console.log('pee')
     }
-
-
 
     return (
 
