9aa752673bb80abfa2b423698e9f5556d0a87e36
SunnySideUp <sunnysideup.official@protonmail.com>
2021-07-14 23:08:00 +0200

    update chad burnt

--- components/pages/home/hero/index.tsx
+++ components/pages/home/hero/index.tsx
@@ -1,8 +1,23 @@
 import tw from "twin.macro"
 import Counter from "../counter"
 import SocialIcon from "../../../social"
+import { ethers } from "ethers"
+import { useState } from "react"
 
 function HeroSection() {
+    const [chadBurnt, setChadBurnt] = useState("loading")
+
+    const getChadBurnt = async () => {
+        const provider = new ethers.providers.JsonRpcProvider("https://rpc.ftm.tools")
+        const CHAD_TOKEN_ADDRESS = "0xcce93540b80abf71b66e0a44fd71e322ce9c4d9e"
+        const ERC20_ABI = [{ "constant": true, "inputs": [], "name": "name", "outputs": [{ "name": "", "type": "string" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "_spender", "type": "address" }, { "name": "_value", "type": "uint256" }], "name": "approve", "outputs": [{ "name": "", "type": "bool" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "totalSupply", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "_from", "type": "address" }, { "name": "_to", "type": "address" }, { "name": "_value", "type": "uint256" }], "name": "transferFrom", "outputs": [{ "name": "", "type": "bool" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "decimals", "outputs": [{ "name": "", "type": "uint8" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [{ "name": "_owner", "type": "address" }], "name": "balanceOf", "outputs": [{ "name": "balance", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "symbol", "outputs": [{ "name": "", "type": "string" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "_to", "type": "address" }, { "name": "_value", "type": "uint256" }], "name": "transfer", "outputs": [{ "name": "", "type": "bool" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [{ "name": "_owner", "type": "address" }, { "name": "_spender", "type": "address" }], "name": "allowance", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "payable": true, "stateMutability": "payable", "type": "fallback" }, { "anonymous": false, "inputs": [{ "indexed": true, "name": "owner", "type": "address" }, { "indexed": true, "name": "spender", "type": "address" }, { "indexed": false, "name": "value", "type": "uint256" }], "name": "Approval", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "name": "from", "type": "address" }, { "indexed": true, "name": "to", "type": "address" }, { "indexed": false, "name": "value", "type": "uint256" }], "name": "Transfer", "type": "event" }]
+        const chadMasterContract = new ethers.Contract(CHAD_TOKEN_ADDRESS, ERC20_ABI, provider);
+        const chadBurnt = await chadMasterContract.balanceOf("0x0000000000000000000000000000000000000001")
+        let chadBurtFormatted = ethers.utils.formatEther(chadBurnt) + " chad"
+        setChadBurnt(chadBurtFormatted)
+    }
+
+    let burn = getChadBurnt()
     return (
         <div tw="flex relative flex-1 items-center justify-center  py-12 px-24 bg-gray-300">
             <div tw="background-color[#004FCE] rounded-3xl h-full w-full font-family[tempest] py-8">
@@ -19,7 +34,7 @@ function HeroSection() {
                             <span tw="text-3xl text-white">Fucking TVL</span>
                             <Counter value="$12,000,000.00" />
                             <span tw="text-3xl text-white">Fucking $Chad Burnt</span>
-                            <Counter value="120,000.00" />
+                            <Counter value={chadBurnt} />
                         </div>
                     </div>
                 </div>
