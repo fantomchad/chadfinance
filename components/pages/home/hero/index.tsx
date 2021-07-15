3fe1c2cad02aee3b339d9ca0b5911b8b8a77aff4
exitonoff <exitonoffdev@gmail.com>
2021-07-15 17:12:15 +0300

    Add formatter for chad burnt

--- components/pages/home/hero/index.tsx
+++ components/pages/home/hero/index.tsx
@@ -16,11 +16,7 @@ function HeroSection() {
     }, [lpFarms, singleStakeFarms, prices])
 
     const calculateTvl = async () => {
-      if (lpFarms && singleStakeFarms && prices) {
-        console.log("LPF", lpFarms)
-        console.log("SSF", singleStakeFarms)
-        console.log("PRI", prices)
-  
+      if (lpFarms && singleStakeFarms && prices) {  
         const lpTokens = lpFarms.map(f => f.pool.stakedToken)
         const singleTokens = singleStakeFarms.map(f => f.pool.stakedToken)
         const allTokens = [...lpTokens, ...singleTokens]
@@ -38,8 +34,8 @@ function HeroSection() {
         const ERC20_ABI = [{ "constant": true, "inputs": [], "name": "name", "outputs": [{ "name": "", "type": "string" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "_spender", "type": "address" }, { "name": "_value", "type": "uint256" }], "name": "approve", "outputs": [{ "name": "", "type": "bool" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "totalSupply", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "_from", "type": "address" }, { "name": "_to", "type": "address" }, { "name": "_value", "type": "uint256" }], "name": "transferFrom", "outputs": [{ "name": "", "type": "bool" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "decimals", "outputs": [{ "name": "", "type": "uint8" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [{ "name": "_owner", "type": "address" }], "name": "balanceOf", "outputs": [{ "name": "balance", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "symbol", "outputs": [{ "name": "", "type": "string" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "_to", "type": "address" }, { "name": "_value", "type": "uint256" }], "name": "transfer", "outputs": [{ "name": "", "type": "bool" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [{ "name": "_owner", "type": "address" }, { "name": "_spender", "type": "address" }], "name": "allowance", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "payable": true, "stateMutability": "payable", "type": "fallback" }, { "anonymous": false, "inputs": [{ "indexed": true, "name": "owner", "type": "address" }, { "indexed": true, "name": "spender", "type": "address" }, { "indexed": false, "name": "value", "type": "uint256" }], "name": "Approval", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "name": "from", "type": "address" }, { "indexed": true, "name": "to", "type": "address" }, { "indexed": false, "name": "value", "type": "uint256" }], "name": "Transfer", "type": "event" }]
         const chadMasterContract = new ethers.Contract(CHAD_TOKEN_ADDRESS, ERC20_ABI, provider);
         const chadBurnt = await chadMasterContract.balanceOf("0x0000000000000000000000000000000000000001")
-        let chadBurtFormatted = ethers.utils.formatEther(chadBurnt) 
-        setChadBurnt(chadBurtFormatted)
+        let chadBurtFormatted = parseFloat(ethers.utils.formatEther(chadBurnt)) 
+        setChadBurnt(chadBurtFormatted.toLocaleString('en-US', {}))
     }
 
     return (
